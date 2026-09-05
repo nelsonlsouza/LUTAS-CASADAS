b0d94856d58f44e0e15c625083d7af95ea4068d6
Nelson Souza <nelsonsouza0328@gmail.com>
2026-09-05 01:30:08 -0400

    feat: adiciona expansão dos cards de luta

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import './styles/fight-expand.css';
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
